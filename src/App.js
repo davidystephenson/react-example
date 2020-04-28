b17251ddcb4d15537be42217b8eb10e9908eded2
David Y. Stephenson <david@davidystephenson.com>
2020-04-28 14:06:44 +0200

    Display examples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
+// const react = require('react')
 import logo from './logo.svg';
 import './App.css';
 
+function Message () {
+  return <p>Welcome students</p>
+}
+
+// <Message />
+// Message()
+
+function say (phrase, punctuation) {
+  return `${phrase}${punctuation}`
+}
+
+say('Hello', '!')
+// Hello!
+
+function Greet (props) {
+  console.log('props test:', props)
+
+  const punctuation = '!'
+
+  const greeting = props.greeting || 'Hello'
+
+  return <p>{greeting} {props.student}{punctuation}</p>
+}
+
+function Greetings2 ({ custom, subtitle }) {
+  return <div>
+    <h3>{subtitle}</h3>
+    <Greet student='Amy' greeting={custom} />
+    <Greet student='Donald' />
+  </div>
+}
+
+function Greetings2 (props) {
+  const { custom, subtitle } = props
+
+  return <div>
+    <h3>{subtitle}</h3>
+    <Greet student='Amy' greeting={custom} />
+    <Greet student='Donald' />
+  </div>
+}
+
+function Greetings3 (props) {
+  return <div>
+    <h3>{props.subtitle}</h3>
+    <Greet student='Amy' greeting={props.custom} />
+    <Greet student='Donald' />
+  </div>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Message />
+
+      <a href='http://google.com'>google</a>
+
+      <Greetings
+        subtitle='I hope you all are welcome'
+        custom="What's up my good friend"
+      />
     </div>
   );
 }
